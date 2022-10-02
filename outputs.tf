8fcba15099a99e7a5fdb1bc45a33f4a760c46f06
Loic Salaris <loic.salaris@swile.co>
2022-10-02 17:03:09 +0200

    :sparkles: feat: define a web-adress output to get public DNS of the created EC2

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,13 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.app_server.id
+  value       = aws_instance.web.id
 }
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
+  value       = aws_instance.web.public_ip
 }
 
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
+}

02696f5cb15cd37477c2529b2140d5f38f26f41c
Loic Salaris <loic.salaris@swile.co>
2022-10-02 16:59:13 +0200

    :lipstick: chore: rename the instance

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "instance_name" {
   description = "Value of the name tag for the EC2 instance"
   type        = string
-  default     = "ExampleAppServerInstance"
+  default     = "ec2 lab"
 }
 

19675242ac8016c0f191b7ef5cf180eb3df308b9
Loic Salaris <loic.salaris@swile.co>
2022-10-02 17:02:35 +0200

    :sparkles: feat: replace hard coded AMI to get most recent hvm ssd images and create a security group with ingress & egress policies

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 terraform {
+
   cloud {
     organization = "labs-training"
     workspaces {
@@ -11,21 +12,60 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
   }
-
   required_version = ">= 1.2.0"
+
 }
 
 provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-08d70e59c07c61a3a"
-  instance_type = "t2.micro"
+resource "random_pet" "sg" {}
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
   tags = {
     Name = var.instance_name
   }
+  user_data = file("./scripts/setup.sh")
 }
 
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.sg.id}-sg"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}

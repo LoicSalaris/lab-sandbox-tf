730cd439197746781b88bd326bebadfd5dcf21d6
Loic Salaris <loic.salaris@swile.co>
2022-10-03 13:00:35 +0200

    :lock: fix: https://github.com/LoicSalaris/lab-sandbox-tf/security/code-scanning/8

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -167,7 +167,7 @@ resource "aws_security_group" "allowHttp" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/24"]
   }
 
   egress {
